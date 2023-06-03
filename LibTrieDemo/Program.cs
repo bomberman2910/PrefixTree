9e5a2c9a8adc60073ead1a1432f6191fc5c78288
Marcel Hasse <bomberman2995@gmail.com>
2023-06-03 23:53:15 +0200

    Added demo project

---
+++ LibTrieDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using LibTrie;
+
+namespace LibTrieDemo;
+
+public class Program
+{
+    private static string currentInput = string.Empty;
+
+    private static Trie trie = new Trie();
+
+    public static void Main(string[] args)
+    {
+        if(args.Length != 1)
+        {
+            Console.WriteLine("Missing word file path");
+            Environment.Exit(1);
+        }
+        if(!File.Exists(args[0]))
+        {
+            Console.WriteLine("Invalid path for word file");
+            Environment.Exit(1);
+        }
+        var words = File.ReadAllLines(args[0]);
+        Console.Write($"Loading words...");
+        var counter = 0;
+        foreach (var word in words)
+        {
+            if(string.IsNullOrEmpty(word))
+                continue;
+            counter++;
+            trie.AddWord(word);
+        }
+        Console.WriteLine("done");
+        Console.WriteLine($"Inserted {counter} valid words");
+        Console.Clear();
+        var readKey = Console.ReadKey();
+        while (readKey.Key != ConsoleKey.Escape)
+        {
+            if (readKey.Key == ConsoleKey.Backspace && currentInput.Length > 0)
+            {
+                currentInput = currentInput.Remove(currentInput.Length - 1);
+                Console.Write(' ');
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            }
+            if (char.IsLetter(readKey.KeyChar))
+                currentInput += readKey.KeyChar;
+            else if(readKey.Key != ConsoleKey.Backspace)
+            {
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                Console.Write(' ');
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            }
+            var savedCursor = Console.GetCursorPosition();
+            Console.SetCursorPosition(0, savedCursor.Top + 1);
+            for(var i = 0; i < 10; i++)
+                Console.WriteLine(new string(' ', 30));
+            Console.SetCursorPosition(0, savedCursor.Top + 1);
+            var wordsForInput = trie.GetWordsFor(currentInput);
+            for(var i = 0; i < (wordsForInput.Count > 10 ? 10 : wordsForInput.Count); i++)
+                Console.WriteLine(wordsForInput[i]);
+            Console.SetCursorPosition(savedCursor.Left, savedCursor.Top);
+            readKey = Console.ReadKey();
+        }
+        Console.Clear();
+    }
+}
--- README.md
+++ README.md
@@ -4,10 +4,6 @@
 
 Basic implementation of a prefix tree (also called Trie) in C# for use in other projects.
 
-## Description
-
-TODO write description
-
 ## Getting Started
 
 ### Dependencies
@@ -20,6 +16,17 @@ TODO write description
 * add LibTrie to your project
 * create a new ```Trie``` instance with ```var trie = new Trie();```
 * add new words to it with ```trie.AddWord("example");```
+* to get all words starting with a certain substring, use ```List<string> words = trie.GetWordsFor("prefix");```
+  * if there are no words in the trie beginning with that substring the method will return an empty list
+
+### Demo app
+
+```LibTreeDemo``` is a small console app showcasing an example for using this library. There are two word lists included with the project, but you can use any list as long as the words contain only letters and are all separated by line breaks. To use the application you have to give the path to your word list as an argument. After loading the words you'll be presented with an empty console expecting you to type characters. After entering a character up to ten words contained inside the Trie, that starting with your input, will be displayed underneath the input, kind of mimicking an autocomplete textbox.
+
+#### Sources for word list files
+
+* [MIT's 10000-word list](https://www.mit.edu/~ecprice/wordlist.10000)
+* [dwyl/english-words on GitHub](https://github.com/dwyl/english-words)
 
 ## Authors
 
